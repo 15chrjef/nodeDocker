7123b896de1bc5002d1aa75fea1b23084e0429dd
jeff christian <jchri@MacBook-Pro-2.loca>
2017-01-30 20:35:47 -0800

    docker node complete

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 8080;
+
+const app = express();
+
+app.get('/', function(req,res) {
+	res.send('Hello World \n')
+});
+
+
+app.listen(PORT);
+console.log('running on localhost ' + PORT);
